876f0c559fb042800d7daa57858795fbf58aa37e
takaya <sample@email.com>
2025-01-27 22:40:12 +0900

    fix/vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,11 @@ import fs from 'fs';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: 'localhost',
     https: {
       key: fs.readFileSync('./key.pem'),
       cert: fs.readFileSync('./cert.pem'),
     },
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
-  optimizeDeps: {
-    include: ['pdfjs-dist'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/pdfjs-dist/],
-    },
+    port: 5173,
+    host: 'localhost',
   },
 });
