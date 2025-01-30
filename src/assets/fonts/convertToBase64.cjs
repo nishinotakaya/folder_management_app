b7dec9715bf98b60a370fe64cd8e5f6b82a7b5e1
takaya <sample@email.com>
2025-01-30 22:35:26 +0900

    fix/絶対パスの指定

--- src/assets/fonts/convertToBase64.cjs
+++ src/assets/fonts/convertToBase64.cjs
@@ -1,8 +1,14 @@
 const fs = require('fs');
-const path = '/Users/nishinotakaya/1.フロントコース_カリキュラムチェック/ゆうと向けのシステム/folder_Management/src/assets/fonts/NotoSansJP-Regular.ttf';
+const path = require('path');
 
-fs.readFile(path, (err, data) => {
-  if (err) throw err;
+// 絶対パスを構築
+const fontPath = path.resolve(__dirname, 'NotoSansJP-Regular.ttf');
+
+fs.readFile(fontPath, (err, data) => {
+  if (err) {
+    console.error(`フォントファイルが見つかりません: ${fontPath}`);
+    throw err;
+  }
   const base64 = data.toString('base64');
   console.log(base64);
 });
